d5f65c7a1906af9b41cf18ce6664ccb9ea0d5030
Marc Doerflinger <marc@doerf.li>
2024-09-27 15:44:15 +0000

    cleanup for build

--- src/app/api/purchase/route.ts
+++ src/app/api/purchase/route.ts
@@ -1,18 +1,17 @@
-import { NextRequest } from "next/server";
-import { LOGGER } from "../../../utils/logger_backend";
 import { nanoid } from "nanoid";
+import { LOGGER } from "../../../utils/logger_backend";
 
 /**
  * purchase protection for a flight
  */
-export async function GET(request: NextRequest, { params } : { params: { carrier: string, flightNumber: string, /* TODO: additional fields */} }) {
+export async function GET(/*request: NextRequest, { params } : { params: { carrier: string, flightNumber: string,  TODO: additional fields } }*/ ) {
     // TODO: 0.1 adapt path to match with parameters required 
     // TODO: 0.2 switch to POST request
 
     const reqId = nanoid();
-    const carrier = params.carrier;
-    const flightNumber = params.flightNumber;
-    LOGGER.debug(`[${reqId} purchase protection for ${carrier} ${flightNumber}`);
+    // const carrier = params.carrier;
+    // const flightNumber = params.flightNumber;
+    LOGGER.debug(`[${reqId} purchase protection `);
 
     // TODO: 0. check balance for purchaser wallet and ensure enough tokens are available for payment
     // TODO: 1. call smart contract createPolicy function
