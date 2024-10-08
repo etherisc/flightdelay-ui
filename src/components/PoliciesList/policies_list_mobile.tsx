d537293581e091faf67c07e00255c258b05f0149
Marc Doerflinger <marc@doerf.li>
2024-10-08 10:13:19 +0000

    show mobile departure and arrival times (#61)

--- src/components/PoliciesList/policies_list_mobile.tsx
+++ src/components/PoliciesList/policies_list_mobile.tsx
@@ -7,6 +7,7 @@ import { useTranslation } from "react-i18next";
 import { FlightPlan } from "../../types/flight_plan";
 import { PolicyData } from "../../types/policy_data";
 import { RiskData } from "../../types/risk_data";
+import { dayjs } from "../../utils/date";
 
 export default function PoliciesListMobile({ policies, risks, loading }: { policies: PolicyData[], risks: RiskData[], loading: boolean }) {
 
@@ -26,11 +27,11 @@ export default function PoliciesListMobile({ policies, risks, loading }: { polic
 function Policy({ policy, risk }: { policy: PolicyData, risk: RiskData | undefined }) {
     const { t } = useTranslation();
 
-    function formatState(flightData: FlightPlan) {
+    function formatState(flightPlan: FlightPlan) {
         let text;
         let color = grey[900] as string;
         const additional = undefined;
-        const state = flightData.status;
+        const state = flightPlan.status;
         switch (state) {
             case 'S': // scheduled
                 text = t('flight_state.scheduled');
@@ -81,6 +82,14 @@ function Policy({ policy, risk }: { policy: PolicyData, risk: RiskData | undefin
         </Box>);
     }
     
+    let departure = undefined;
+    let arrival = undefined;
+
+    if (risk.flightPlan !== null) {
+        departure = dayjs.tz(risk.flightPlan.departureTimeLocal, risk.flightPlan.departureTimeLocalTimezone!);
+        arrival = dayjs.tz(risk.flightPlan.arrivalTimeLocal, risk.flightPlan.arrivalTimeLocalTimezone!);
+    }
+
     return (<Box sx={{ pb: 1, mb: 1, borderBottom: '1px solid', borderBottomColor: grey[300] }}>
         <Grid container spacing={1}>
             <Grid size={6}>
@@ -89,17 +98,11 @@ function Policy({ policy, risk }: { policy: PolicyData, risk: RiskData | undefin
             <Grid size={6} sx={{ textAlign: 'right'}}>
                 <b>{t('flight')}: {risk.carrier} {risk.flightNumber}</b>
             </Grid>
-            {/* <Grid size={12}>
-                <FontAwesomeIcon icon={faPlaneDeparture} /> {t('from')} {risk.flightPlan?.departureAirportFsCode} @ {formatTime(risk.flightPlan?.departureTimeUtc)}
+            <Grid size={12}>
+                <FontAwesomeIcon icon={faPlaneDeparture} /> {t('from')} {risk.flightPlan?.departureAirportFsCode} @ {departure?.format('HH:mm')}
             </Grid>
             <Grid size={12}>
-                <FontAwesomeIcon icon={faPlaneArrival} /> {t('to')} {risk.flightPlan?.arrivalAirportFsCode} @ {formatTime(risk.flightPlan?.arrivalTimeUtc)}
-            </Grid> */}
-            <Grid size={6}>
-                <FontAwesomeIcon icon={faPlaneDeparture} /> {t('from')} <b>{risk.flightPlan?.departureAirportFsCode}</b>
-            </Grid>
-            <Grid size={6}>
-                <FontAwesomeIcon icon={faPlaneArrival} /> {t('to')} <b>{risk.flightPlan?.arrivalAirportFsCode}</b> 
+                <FontAwesomeIcon icon={faPlaneArrival} /> {t('to')} {risk.flightPlan?.arrivalAirportFsCode} @ {arrival?.format('HH:mm')}
             </Grid>
             <Grid size={12}>
                 {formatState(risk.flightPlan!)}
