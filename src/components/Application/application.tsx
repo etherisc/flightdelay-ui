85f53929a668758bb764af4e78774bf67f174ba7
Marc Doerflinger <marc@doerf.li>
2024-10-24 11:39:55 +0000

    Refactor Application component and add PurchaseExecutionModal

--- src/components/Application/application.tsx
+++ src/components/Application/application.tsx
@@ -1,31 +1,32 @@
 import { faCartShopping, faWallet } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Alert, Box, Card, CardActions, CardContent, CardHeader, CircularProgress, LinearProgress, Modal, SvgIcon, Theme, Typography, useMediaQuery } from "@mui/material";
-import { grey } from "@mui/material/colors";
+import { Box, Card, CardActions, CardContent, CardHeader, LinearProgress, SvgIcon, Theme, Typography, useMediaQuery } from "@mui/material";
 import { useEnvContext } from "next-runtime-env";
 import Image from "next/image";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { useWallet } from "../../hooks/onchain/use_wallet";
 import useApplication from "../../hooks/use_application";
 import { setAirportWhitelist } from "../../redux/slices/flightData";
 import { RootState } from "../../redux/store";
-import { formatAmount } from "../../utils/amount";
 import Button from "../Button/button";
 import Trans from "../Trans/trans";
 import { ApplicationError } from "./application_error";
 import ApplicationForm from "./application_form";
 import FlightData from "./flight_data";
+import PurchaseExecutionModal from "./purchase_execution_modal";
 import PurchaseSuccess from "./purchase_success";
 
 export default function Application() {
     const { t } = useTranslation();
     const isSmallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down("md"));
     const { connectWallet } = useWallet();
-    const { NEXT_PUBLIC_AIRPORTS_WHITELIST, NEXT_PUBLIC_AIRPORTS_BLACKLIST, NEXT_PUBLIC_PREMIUM_TOKEN_SYMBOL } = useEnvContext();
+    const { NEXT_PUBLIC_AIRPORTS_WHITELIST, NEXT_PUBLIC_AIRPORTS_BLACKLIST } = useEnvContext();
     const dispatch = useDispatch();
     const { purchaseProtection, fetchRiskpoolCapacity } = useApplication();
+
+    // const [ showPurchaseSuccess, setShowPurchaseSuccess ] = useState(false);
 
     const flightDataState = useSelector((state: RootState) => state.flightData);
     const walletIsConnected = useSelector((state: RootState) => state.wallet.isConnected);
@@ -84,46 +85,8 @@ export default function Application() {
         </Button>;
     }
 
-    const executePurchase = 
-            <Modal
-                open={true}
-                aria-labelledby="loading-modal-title"
-                aria-describedby="loading-modal-description"
-                sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: grey[200] + `80` }}
-            >
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        bgcolor: "white",
-                        opacity: '1.0 !important',
-                        boxShadow: 24,
-                        p: 4,
-                        borderRadius: 1,
-                        zIndex: 1000
-                    }}
-                >
-                    <CircularProgress />
-                    <Box component="span" sx={{ mt: 2 }} maxWidth="md">
-                        { ! executingSigning && <Box sx={{ mb: 2, textAlign: 'center' }}>
-                            <Trans k="purchasing" />
-                        </Box>}
-                        { executingSigning && <Box sx={{ mb: 2, textAlign: 'center' }}>
-                            <Alert severity="info">
-                                <Trans k="signing_request_1" />
-                                <br />
-                                <Trans k="signing_request_2" values={{ symbol: NEXT_PUBLIC_PREMIUM_TOKEN_SYMBOL, amount: formatAmount(BigInt(flightDataState.premium!), 6, 0)}} >
-                                    <b></b>
-                                </Trans>
-                            </Alert>
-                        </Box>}
-                    </Box>
-                </Box>
-            </Modal>
-
     return (<>
+        <PurchaseExecutionModal executingPurchase={executingPurchase} executingSigning={executingSigning} premium={flightDataState.premium} />
         <Card>
             <CardHeader
                 avatar={
@@ -142,8 +105,7 @@ export default function Application() {
                 <Actions 
                     button={button} 
                     executingPurchase={executingPurchase} 
-                    purchaseSuccessful={purchaseSuccessful} 
-                    executePurchase={executePurchase} />
+                    purchaseSuccessful={purchaseSuccessful} />
             </CardActions>
         </Card>
     </>);
@@ -157,10 +119,9 @@ function Instructions() {
         </Box>;
 }
 
-function Actions({ button, executingPurchase, purchaseSuccessful, executePurchase }: { button: JSX.Element, executingPurchase: boolean, purchaseSuccessful: boolean, executePurchase: JSX.Element }) {
+function Actions({ button, executingPurchase, purchaseSuccessful }: { button: JSX.Element, executingPurchase: boolean, purchaseSuccessful: boolean }) {
     return <>
             {(!executingPurchase && !purchaseSuccessful) && button}
-            {executingPurchase && executePurchase}
             {(!executingPurchase && purchaseSuccessful) && <PurchaseSuccess />}
             <Box sx={{ py: 2 }}>
                 <Typography variant="body2" component="p" color="textSecondary">
