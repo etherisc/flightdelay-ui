4cae7f52fa8f4b0268c555e719a922ade1eaf820
Marc Doerflinger <marc@doerf.li>
2024-10-29 09:51:18 +0000

    cleanup unused features

--- src/utils/features.ts
+++
@@ -1,9 +0,0 @@
-import { Features } from "../types/features";
-
-export function features() {
-    return { 
-        locationSearch: process.env.LOCATION_IQ_API_KEY !== undefined,
-        geoLocationApi: process.env.IP_GEOLOCATION_API_KEY !== undefined,
-        csp: process.env.CSP_ENABLED?.toLowerCase() === 'true',
-    } as Features;
-}
